6bbca02f3123b713d36b61b1c8e6fc9d967ccfb1
mabbuprem <mabbupremkumar@gmail.com>
2022-04-03 08:03:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Registration_Regex
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationPattern userRegistrationPattern = new UserRegistrationPattern();
+            string[] checkingFirstNames = { "Prem","Pop","Ho","prem" };
+            string[] checkingLastNames = { "Kumar", "khan", "So", "kumaran" };
+            string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
+            string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
+            string[] checkingPassword = { "bdvhvhv8sBbs", "91720659414@9A", "vvvccbhcbh8", "hcvxhvzA8" };
+            string[] checkingEmails2 = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+            userRegistrationPattern.ValidateFirstName(checkingFirstNames);
+            userRegistrationPattern.ValidatelastName(checkingLastNames);
+            userRegistrationPattern.ValidateeMail(checkingEMails);
+            userRegistrationPattern.ValidateMobileNo(checkingNo);
+            userRegistrationPattern.ValidatePassword(checkingPassword);
+            userRegistrationPattern.ValidateeMail(checkingEmails2);
+        }
+    }
+}
---
+++ UserRegistationPattern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Regex
+{
+    public class UserRegistrationPattern
+    {
+        public static string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public static string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public static string eMail = @"^[a-z]{3,}([.]{1}[a-z]{2,})?@bl.co([.]{1}[a-z]{2})?$";
+        public static string mobileNoFormat = @"^91[ ][5-9]{1}[0-9]{9}$";
+        public static string passwordFormat = "^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z1-9]{1}[a-zA-Z0-9]{7,}";
+        public static string eMail2 = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
+        public void ValidatePassword(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, passwordFormat) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+                Console.WriteLine(result);
+            }
+        }
+        public void ValidateMobileNo(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, mobileNoFormat) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+                Console.WriteLine(result);
+            }
+        }
+        public void ValidateeMail(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, eMail2) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+                Console.WriteLine(result);
+            }
+        }
+        public void ValidatelastName(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, lastName) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+                Console.WriteLine(result);
+            }
+        }
+        public void ValidateFirstName(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, firstName) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
