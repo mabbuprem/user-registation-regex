f62a892a618342d18fe5eb5146eb6d829956c3e4
mabbuprem <mabbupremkumar@gmail.com>
2022-04-03 08:49:45 +0530

    [prem] At least one Numeric in Password

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace User_Registration_Regex
         {
             UserRegistrationPattern userRegistrationPattern = new UserRegistrationPattern();
             string[] checkingFirstNames = { "Prem", "Pop", "Ho", "prem" };
-            string[] checkingLastNames = { "Kumar", "khan", "So", "kumarn" };
+            string[] checkingLastNames = { "Kumar", "khan", "So", "kumar" };
             string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
             string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
-            string[] checkingPassword = { "A222hvhv2626", "917206594149A", "910720659414", "91Ab0659c1491" };
+            string[] checkingPassword = { "Abdvhvhvsbs", "917206594149A", "vvvccbhcbh8", "hcvxhvzA8" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
             userRegistrationPattern.ValidatelastName(checkingLastNames);
             userRegistrationPattern.ValidateeMail(checkingEMails);
