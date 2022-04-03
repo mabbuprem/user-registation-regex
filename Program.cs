3aa551b33f264d41211234650a4b6cfd689a57ec
mabbuprem <mabbupremkumar@gmail.com>
2022-04-03 08:20:08 +0530

    validate last name

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace User_Registration_Regex
         {
             UserRegistrationPattern userRegistrationPattern = new UserRegistrationPattern();
             string[] checkingFirstNames = { "Prem", "Pop", "Ho", "prem" };
+            string[] checkingLastNames = { "Kumar", "khan", "So", "kumaran" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
+            userRegistrationPattern.ValidatelastName(checkingLastNames);
         }
     }
 }
--- UserRegistationPattern.cs
+++ UserRegistationPattern.cs
@@ -10,7 +10,16 @@ namespace User_Registration_Regex
     internal class UserRegistrationPattern
     {
         public static string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
-     
+        public static string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public void ValidatelastName(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, lastName) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+
+                Console.WriteLine(result);
+            }
+        }
         public void ValidateFirstName(string[] names)
         {
             foreach (string n in names)
@@ -19,7 +28,6 @@ namespace User_Registration_Regex
 
                 Console.WriteLine(result);
             }
-
         }
     }
 }
