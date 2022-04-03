8f3792f8f7cdc8af91f89551b4d7bf66e5f3341a
mabbuprem <mabbupremkumar@gmail.com>
2022-04-03 08:14:22 +0530

    validate user first name

--- Program.cs
+++ Program.cs
@@ -5,23 +5,13 @@ using System.Threading.Tasks;
 
 namespace User_Registration_Regex
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             UserRegistrationPattern userRegistrationPattern = new UserRegistrationPattern();
-            string[] checkingFirstNames = { "Prem","Pop","Ho","prem" };
-            string[] checkingLastNames = { "Kumar", "khan", "So", "kumaran" };
-            string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
-            string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
-            string[] checkingPassword = { "bdvhvhv8sBbs", "91720659414@9A", "vvvccbhcbh8", "hcvxhvzA8" };
-            string[] checkingEmails2 = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+            string[] checkingFirstNames = { "Prem", "Pop", "Ho", "prem" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
-            userRegistrationPattern.ValidatelastName(checkingLastNames);
-            userRegistrationPattern.ValidateeMail(checkingEMails);
-            userRegistrationPattern.ValidateMobileNo(checkingNo);
-            userRegistrationPattern.ValidatePassword(checkingPassword);
-            userRegistrationPattern.ValidateeMail(checkingEmails2);
         }
     }
 }
